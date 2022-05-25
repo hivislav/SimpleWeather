2fad835f4ee3bfcfd01bb3fd574c45b8a4d405b8
hivislav <hivislav@gmail.com>
2022-05-25 12:55:38 +0500

    0.1 Созданы макеты фрагмента детали, активити и основные ресурсы (dimens, strings)

--- app/src/main/java/ru/hivislav/simpleweather/view/MainActivity.kt
+++ app/src/main/java/ru/hivislav/simpleweather/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.hivislav.simpleweather
+package ru.hivislav.simpleweather.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.hivislav.simpleweather.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
